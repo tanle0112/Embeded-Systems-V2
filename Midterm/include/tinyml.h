b8bdfe89059d0cddb43c27a123b5532ed478da76
nhavi0105 <n.havii0105@gmail.com>
2025-11-03 21:20:04 +0700

    Update tinyml.h
    Signed-off-by: nhavi0105 <n.havii0105@gmail.com>

--- Midterm/include/tinyml.h
+++ Midterm/include/tinyml.h
@@ -1,19 +1,22 @@
-#ifndef __TINY_ML__
-#define __TINY_ML__
+#pragma once
+#ifndef __TINYML_H__
+#define __TINYML_H__
 
 #include <Arduino.h>
-
 #include "dht_anomaly_model.h"
-#include "global.h"
+#include "config.h"
+#include "leds.h"
+#include "temp_humi.h"
 
+// TensorFlow Lite Micro headers
 #include <TensorFlowLite_ESP32.h>
 #include "tensorflow/lite/micro/all_ops_resolver.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
-#include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
 
 void setupTinyML();
-void tiny_ml_task(void *pvParameters);
+void TaskTinyML(void *pvParameters);
 
 #endif
